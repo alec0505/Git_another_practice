89ba39a1b7b2e7171b029814673b3480ea07d3bd
jonatahrokachov <jonatah.rokachov29@gmail.com>
2022-09-06 19:20:04 -0400

    wooden Spoon

---
+++ src/Jonata_packeg/WoodenSpoon.java
@@ -0,0 +1,11 @@
+package Jonata_packeg;
+
+public class WoodenSpoon {
+    public static void main(String[] args) {
+        int commit = 4;
+        if(commit >= 3 ){
+            System.out.println("Good Job");
+        }
+    }
+
+}
